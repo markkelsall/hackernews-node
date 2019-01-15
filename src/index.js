084f8f8b889c83291d51fce9a6b928d7eba110dc
Mark Kelsall <mark.kelsall@gmail.com>
2019-01-15 07:42:46 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+let links = [
+    {
+        id: 'link-0',
+        url: 'www.howtographql.com',
+        description: 'Fullstack tutorial for GraphQL'
+    }
+];
+
+// 2
+const resolvers = {
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+    },
+    Link: {
+        id: parent => parent.id,
+        description: parent => parent.description,
+        url: parent => parent.url,
+    }
+};
+
+// 3
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
